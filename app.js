70acccb43d135cb14212875a130f5490d19e8a74
Kevin <NateraKevin1@gmail.com>
2022-07-03 17:50:53 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require('mongoose');
+const app = express();
+const db = require('./config/keys').mongoURI;
+const passport = require('passport');
+
+
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
+
+
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+
+// app.get("/", (req, res) => res.send("Hello Universe"));
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
+
+const port = process.env.PORT || 5001;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
